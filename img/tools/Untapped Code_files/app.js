01ec5d25f5aa89c861386edfddfd8fd45cbdc7fd
AustinMorales <54542173+AustinMorales09@users.noreply.github.com>
2019-09-27 16:00:44 -0400

    Add files via upload

---
+++ img/tools/Untapped Code_files/app.js
@@ -0,0 +1,5 @@
+function outputName() {
+    var x = document.getElementById("frstName").value;
+    var y= document.getElementById("lastName").value;
+    document.getElementById("demo").innerHTML = "Hello "+ x + " " + y;
+}
